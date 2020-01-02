fab3a5cd14998a2ec18cee44560276088d8df061
Stanslaus <mstans8@gmail.com>
2020-01-02 21:04:24 +0300

    Update search view, limit the number of characters in title for uniformity

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -10,6 +10,22 @@ export const clearResults = () => {
     elements.resultsList.innerHTML = '';
 };
 
+const limitRecipeTitle = (title,limit = 17) => {
+    const newTitle = [];
+    if(title.length > limit){
+        title.split(/\W/).reduce((acc,curr) => {
+            if(acc + curr.length <= limit){
+                newTitle.push(curr);
+            }
+            return acc + curr.length;
+
+        },0);
+        // Return the results
+        return newTitle.join(" ").concat('...');
+    }
+    return title;
+}
+
 const renderRecipe = recipe => {
     let imageUrl = `https://spoonacular.com/recipeImages/${recipe.id}-556x370.jpg`;
     const markup = `
@@ -19,7 +35,7 @@ const renderRecipe = recipe => {
                             <img src="${imageUrl}" alt="${recipe.title}">
                         </figure>
                         <div class="results__data">
-                            <h4 class="results__name">${recipe.title}...</h4>
+                            <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                             <p class="results__author">${recipe.readyInMinutes}</p>
                         </div>
                     </a>
